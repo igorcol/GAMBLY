e68336b906617f4f56f9baccb2c344d2e9a9ae0f
igorCol <igor.colombini@gmail.com>
2026-07-15 19:43:17 -0300

    disable unsed vars

--- src/components/ui/Chip.tsx
+++ src/components/ui/Chip.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ButtonHTMLAttributes } from 'react'
 import { motion, HTMLMotionProps } from 'framer-motion'
 
